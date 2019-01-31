d52be7c929d91539977d8b517c43c52392d65248
liudong <liudong@daowoo.com>
2019-01-31 10:54:46 +0800

    fix

--- example/architect/spring-example/0 my-spring/README.md
+++
@@ -1,8 +0,0 @@
-mvn spring-boot:run
-
-## prepare
-```
-cd /mnt/disk/docker/service
-sh server/mariadb.sh 
-使用 Navicat Premium
-```
--- example/architect/spring-example/0 my-spring/src/main/java/com/data/repository/DepartmentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.data.repository;
-
-import com.data.domain.Department;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DepartmentRepository extends JpaRepository<Department, String> {
-}
